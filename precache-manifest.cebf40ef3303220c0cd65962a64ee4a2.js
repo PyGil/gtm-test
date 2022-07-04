5eaa19727b4ac2aa34761368c4383f0573c0e62d
PyGil <samusenko.kirill22@gmail.com>
2022-07-04 13:24:53 +0300

    Updates

---
+++ precache-manifest.cebf40ef3303220c0cd65962a64ee4a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "73b016dd5f95b903ee1c",
+    "url": "/gtm-test/static/css/main.e4674bb8.chunk.css"
+  },
+  {
+    "revision": "73b016dd5f95b903ee1c",
+    "url": "/gtm-test/static/js/main.73b016dd.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/gtm-test/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "45d599a840ff1efe516f",
+    "url": "/gtm-test/static/js/2.45d599a8.chunk.js"
+  },
+  {
+    "revision": "b0fab13909629a7092b7",
+    "url": "/gtm-test/static/js/runtime~main.b0fab139.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/gtm-test/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "c0d29708744def0409be18fbdbb7a75e",
+    "url": "/gtm-test/index.html"
+  }
+];
